6d2986070645094ed9badb4d8a786688e5253815
mohamdabidi2 <45004605+mohamdabidi2@users.noreply.github.com>
2018-12-22 18:59:55 +0100

    Updates

---
+++ precache-manifest.bb766e04ba897f76e8ce68407c928dba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54eaa80ddedea39f0773",
+    "url": "/navbar/static/js/runtime~main.54eaa80d.js"
+  },
+  {
+    "revision": "35beda3120d0003e3ed6",
+    "url": "/navbar/static/js/main.35beda31.chunk.js"
+  },
+  {
+    "revision": "5e3dff33ccbb00a170c2",
+    "url": "/navbar/static/js/1.5e3dff33.chunk.js"
+  },
+  {
+    "revision": "35beda3120d0003e3ed6",
+    "url": "/navbar/static/css/main.d988187f.chunk.css"
+  },
+  {
+    "revision": "19a16289bf0cf7ed0169d31e90217f68",
+    "url": "/navbar/index.html"
+  }
+];
